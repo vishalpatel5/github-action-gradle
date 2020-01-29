faba10fdc56ed515e04ed6a735a64314639bd7ce
Vishal <vishal7@outlook.in>
2020-01-29 21:57:54 +0530

    Initial commit

---
+++ src/main/java/com/github/action/gradle/GithubActionGradleApplication.java
@@ -0,0 +1,12 @@
+package com.github.action.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionGradleApplication.class, args);
+	}
+}
